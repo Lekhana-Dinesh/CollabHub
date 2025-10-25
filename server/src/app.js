47dccf89e57ecfaf4fc73a9589a3c67a80b17ad4
Lekhana-Dinesh <lekhana.24bcs10108@sst.scaler.com>
2025-10-25 21:51:27 +0530

    allow netlify origin in cors

--- server/src/app.js
+++ server/src/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 // CORS
 app.use(
   cors({
-    origin: ["http://localhost:8080", "http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:8080", "http://localhost:5173", "http://localhost:3000", "https://project-name-colabhub.netlify.app"],
     credentials: true,
   })
 );
